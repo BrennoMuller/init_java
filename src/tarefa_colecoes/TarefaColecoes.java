4a3f3a21de0c42bda712a3b164308856a4021b0e
BrennoMuller <brennomillervitorino2016@gmail.com>
2026-04-22 09:46:51 -0300

    tarefa coleções parte 1

---
+++ src/tarefa_colecoes/TarefaColecoes.java
@@ -0,0 +1,55 @@
+package tarefa_colecoes;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TarefaColecoes {
+	
+	public static void part1(String lista){
+		
+		String[] a = lista.split(",");
+		
+		for (String nome : a) {
+		    System.out.println(nome);
+		}
+		
+	}
+	
+	public static void part2(String lista){
+		
+		
+		List<String> masculino = new ArrayList<String>(); 
+		List<String> feminino = new ArrayList<String>(); 
+		
+		String[] a = lista.split(",");
+		
+		for (String nome : a) {
+			
+			if (nome.contains("-m")) {
+				masculino.add(nome);
+			}
+			
+			
+			if (nome.contains("-f")) {
+				feminino.add(nome);
+			}
+			
+		}
+		
+		 System.out.println("masculino");
+		 System.out.println(masculino);
+		 System.out.println("feminino");
+		 System.out.println(feminino);
+		
+	}
+	
+	public static void main(String[] args) {
+		if (args[0].contains("-m") || args[0].contains("-f")) {
+			part2(args[0]);
+		}else {
+			part1(args[0]);
+		}
+
+	}
+	
+
+}
